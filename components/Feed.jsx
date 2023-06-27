5dc9a7ffd0a3565fa44bc8b82e596b364e5c2dbf
Subha Ghanta <subhaghanta60@gmail.com>
2023-06-27 21:01:14 +0530

    Prompt Card

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-
+'use client';
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
 const Feed = () => {
+  const [searchText,setsearchText] =useState('');
+
+  const handleSearchChange = (e)=> {
+
+  }
   return (
-    <div>
-      Feed
-    </div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+        type="text"
+        placeholder="Search for a tag or a username"
+        value={searchText}
+        onChange={handleSearchChange}  
+        required
+        className="search_input peer"
+        />
+      </form>
+      <PromptCard 
+      data={[]}
+      handleTagClick ={() => {}}
+      />
+    </section>
   )
 }
 
