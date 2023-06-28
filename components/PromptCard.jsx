6ccac9554487a1240a99d24a4802119a1c744642
Subha Ghanta <subhaghanta60@gmail.com>
2023-06-28 23:09:13 +0530

    p

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import {useSession} from 'next-auth/react';
 import {usePathname, useRouter} from 'next/navigation';
 
-
+///This is prompt Card section
 
 const PromptCard = ({post,handleTagClick,handleEdit, handleDelete}) => {
   console.log(post); 
@@ -42,7 +42,7 @@ const PromptCard = ({post,handleTagClick,handleEdit, handleDelete}) => {
           />
 
         </div>
-
+    
 
       </div>
       <p className="my-4 font-satoshi text-sm text-sm text-gray-700">
